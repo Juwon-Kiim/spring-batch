97ccecec4bfd4d1bbb85a8ecdafde2d3ca511979
김주원 <sa6812000@naver.com>
2024-08-15 19:27:10 +0900

    create entity and repository

---
+++ SpringBatch/src/main/java/com/my/SpringBatch/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.my.SpringBatch.repository;
+
+import com.my.SpringBatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ SpringBatch/src/main/java/com/my/SpringBatch/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package com.my.SpringBatch.repository;
+
+import com.my.SpringBatch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
